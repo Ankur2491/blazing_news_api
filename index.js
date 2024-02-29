d4b1f57ddc9173bdbae98a9cff92e34cfe9dcd81
Ankur Sharma <ankur.101045@gmail.com>
2024-03-01 01:33:41 +0530

    added gemini API integration

--- index.js
+++ index.js
@@ -214,6 +214,62 @@ app.get("/channels/:channelId", async (req, res) => {
 	let json = await x.json()
 	res.send(json)
 });
+
+const {
+    GoogleGenerativeAI,
+    HarmCategory,
+    HarmBlockThreshold,
+  } = require("@google/generative-ai");
+  
+  const MODEL_NAME = "gemini-1.0-pro";
+  const API_KEY = "AIzaSyBGitFHOcooc4WLc2OOyhH53n6MbIer604";
+  
+  async function runChat() {
+    const genAI = new GoogleGenerativeAI(API_KEY);
+    const model = genAI.getGenerativeModel({ model: MODEL_NAME });
+  
+    const generationConfig = {
+      temperature: 0.9,
+      topK: 1,
+      topP: 1,
+      maxOutputTokens: 2048,
+    };
+  
+    const safetySettings = [
+      {
+        category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+        threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+      },
+      {
+        category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+        threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+      },
+      {
+        category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
+        threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+      },
+      {
+        category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+        threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+      },
+    ];
+  
+    const chat = model.startChat({
+      generationConfig,
+      safetySettings
+    });
+  
+    const result = await chat.sendMessage("some interesting facts");
+    const response = result.response;
+    return response.text();
+  }
+  
+  app.get("/interestingFacts", async (req, res) => {
+    let data = await runChat();
+    let json = {facts: data};
+	res.send(json)
+});
+
 app.listen(port, () => {
     console.log(`Example app is listening on port http://localhost:${port}`)
 });
